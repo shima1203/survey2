dc23d8c12bc368173cd3878f0babc13ede6144be
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-11-29 02:56:13 +0900

    Update

--- .php_tmp/questionnaire1.php
+++ .php_tmp/questionnaire1.php
@@ -54,7 +54,7 @@ if (!empty($_POST)){
     // $message1->execute(array($answer_id, $questionnaire_id, $question['question_id'], $answer));
 
     $message2=$db->prepare('INSERT INTO AnswerData SET answer_id=?, questionnaire_id=?, scroll=?, coordinates=?, click=?, windowsize=?, background=?, checking=?, type=?, enter_leave=?, total=?, created_at=NOW()');
-    $message1->execute(array($answer_id, $questionnaire_id, $_POST["scroll"], $_POST["coordinates"], $_POST["click"], $_POST["background"], $_POST["check"], $_POST["type"], $_POST["enter_leave"], $_POST["total"]));
+    $message2->execute(array($answer_id, $questionnaire_id, $_POST["scroll"], $_POST["coordinates"], $_POST["click"], $_POST["background"], $_POST["check"], $_POST["type"], $_POST["enter_leave"], $_POST["total"]));
   }
   //header('Location: /survey/questionnaire_confirmation.php');
   exit();
