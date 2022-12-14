1db2c5dfd15154bdcec1492dc935659b5662372b
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-15 02:57:34 +0900

    Update

--- mouse_movement.js
+++ mouse_movement.js
@@ -37,7 +37,7 @@ function resize_window(window_x, window_y){
   window.resizeTo(window_x, window_y);
 }
 
-newwindow = open('result_mouse_new.php', 'mywindow', 'width=600,height=400');
+newwindow = open('result_mouse_new.php', null, 'width=600,height=400');
 
 newwindow.document.addEventListener('DOMContentLoaded', function() {
   draw(coordinates);
