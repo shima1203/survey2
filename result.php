59317eb99e32c00211e373c4b282f7bf8a5c24c4
shimamamamamamae <shima.k.1203@gmail.com>
2022-12-14 11:02:53 +0900

    Update

--- result.php
+++ result.php
@@ -126,7 +126,7 @@ if($questionnaire_id == 2 && $ex2 == 0){
 
                 echo "<td>" . $answer['base_data']['created_at'] . "</td>";
                 
-                echo '<td align="center">', '<button onclick="location.href=\'/survey/mouse_result2.php?answer_id=' . $answer['base_data']['answer_id'] . '\'">click here</button>'. "</td>";
+                echo '<td align="center">', '<button onclick="location.href=\'/survey/result_mouse.php?answer_id=' . $answer['base_data']['answer_id'] . '\'">click here</button>'. "</td>";
                 
                 echo "</tr>";
             }
