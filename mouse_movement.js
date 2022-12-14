005cad12ba6ca788b337fa86eb32ea9f55732672
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-15 02:14:37 +0900

    Update

--- mouse_movement.js
+++ mouse_movement.js
@@ -1,5 +1,5 @@
 function draw(coordinates){
-  const canvas = window.document.getElementById("canvas");
+  const canvas = newwindow.document.getElementById("canvas");
     if (canvas.getContext) {
       const context = canvas.getContext("2d");//2次元描画
 
