5dc4af55937649c5e24be1cef80242f43ab52c68
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-23 08:34:49 +0900

    Update

--- analyze.php
+++ analyze.php
@@ -1,8 +1,5 @@
 <?php
-$command = "export LANG=ja_JP.UTF-8;python /home/ubuntu/survey/analyze.py";
-exec($command, $output);
-
-foreach($output as $o){
-    echo $o . '<br>';
-}
+exec("python analyze.py", $output);
+ 
+print_r($output);
 ?>
