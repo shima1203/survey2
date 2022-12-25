2f4a81fa170e353b14d5232dedabeae570a4fc69
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-25 16:08:57 +0900

    Update

--- analyze.php
+++ analyze.php
@@ -1,6 +1,29 @@
 <?php
 require('dbconnect.php');
 
+$questionnaire_id = 3;
+
+//回答
+$answers_pre=$db->prepare('SELECT * FROM Answers');
+$answers_pre->execute(array());
+$answers=$answers_pre->fetchAll();
+//回答データ
+$answerdata_pre=$db->prepare('SELECT * FROM AnswerData');
+$answerdata_pre->execute(array());
+$answerdata=$answerdata_pre->fetch();
+//アンケート情報
+$questionnaires=$db->prepare('SELECT * FROM Questionnaires WHERE questionnaire_id=?');
+$questionnaires->execute(array($questionnaire_id));
+$questionnaire=$questionnaires->fetch();
+//質問情報
+$questions_pre=$db->prepare('SELECT * FROM Questions WHERE questionnaire_id=?');
+$questions_pre->execute(array($questionnaire_id));
+$questions=$questions_pre->fetchAll();
+
+
+
+
+
 exec("python3 analyze.py", $output);
 var_dump($output);
 ?>
