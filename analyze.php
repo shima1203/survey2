f9ebb2e05f3bd1bfc68b191989940795a6f050a3
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-26 06:42:04 +0900

    Update

--- analyze.php
+++ analyze.php
@@ -21,7 +21,7 @@ $questions_pre->execute(array($questionnaire_id));
 $questions=$questions_pre->fetchAll();
 
 
-$click = $answerdata[0]["click"];
+$click = $answerdata[0];
 $test="test";
 $command = "python3 analyze.py $click";
 exec($command, $output);
