978b905cde2f7ded4088cfa02b5b7681cd9c6835
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-26 07:32:20 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -1,8 +1,7 @@
 import sys
+import numpy as np
 import json
 
 test1 = sys.argv[1]
-# test2 = json.loads(sys.argv[1])
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
+test2 = np.array(json.loads(sys.argv[1]))
+print(test1)
