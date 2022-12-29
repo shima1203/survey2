9088d0b631dae050cfab8c6ebf265d5f1650313d
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2022-12-29 15:45:38 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -4,9 +4,9 @@ import MySQLdb
 # データベースへの接続とカーソルの生成
 connection = MySQLdb.connect(
     host='localhost',
-    user='root',
-    passwd='ルートのパスワード',
-    db='python_db')
+    user='ubuntu',
+    passwd='Aquos.123',
+    db='survey')
 cursor = connection.cursor()
 
 # ここに実行したいコードを入力します
