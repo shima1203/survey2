e82d31976adae121fd5d1129a35e2aabbc885fba
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2023-01-02 15:50:35 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -3,7 +3,6 @@ import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline 
 import seaborn as sns; sns.set()
 import warnings
 warnings.filterwarnings('ignore')
