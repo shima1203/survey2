682870a813f66d56be880dede2077cf34f624f78
shimamamamamamae <shima.k.1203@gmail.com>
2023-01-06 01:53:38 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -102,17 +102,17 @@ def mouse_speed_click_pre(coordinates_ori=[], clicks_ori=[]):
     for click in clicks:
         i = 0
         for coordinate in coordinates:
-            print(coordinate['time'])
+            # print(coordinate['time'])
             if(coordinate['time'] <= click['time'] and coordinate['time'] >= click['time'] - time_close):
                 if(coordinate['time'] - time_tmp <= 1000):
                     click_pre_list.append(coordinate['time'] - time_tmp)
                     tmp.append(coordinate['time'])
-            if(coordinate['time'] > click['time']):
-                break
+            # if(coordinate['time'] > click['time']):
+            #     break
             
-            time_tmp = coordinate['time']
-            del coordinates[i]
-            i += 1
+            # time_tmp = coordinate['time']
+            # del coordinates[i]
+            # i += 1
         print('click_pre_list : ', len(click_pre_list))
         print('coordinates_list : ', len(coordinates))
         print('----------------------------------')
