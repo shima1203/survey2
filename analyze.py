bf44cb940225ba25719b0658f60058026e8cd645
shimamamamamamae <shima.k.1203@gmail.com>
2023-01-06 02:26:01 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -114,6 +114,7 @@ def mouse_speed_click_pre(coordinates_ori=[], clicks_ori=[]):
         print('i : ', i)
         print('len : ', len(coordinates))
         for j in range(i):
+            print('j : ', j)
             print('del : ',coordinates[j]['time'])
             del coordinates[j]
         print('click_pre_list : ', len(click_pre_list))
