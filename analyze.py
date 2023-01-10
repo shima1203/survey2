8b9c3c3fce2d7ff7ca829869c604e86e3e543702
shimamamamamamae <shima.k.1203@gmail.com>
2023-01-10 14:08:51 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -97,7 +97,7 @@ def mouse_speed_click_pre(coordinates_ori=[], clicks_ori=[]):
     clicks = clicks_ori.copy()
     click_pre_list = []
     tmp = []
-    time_close = 100
+    time_close = 500
     time_tmp = coordinates[0]['time']
     for click in clicks:
         i = 0
@@ -133,6 +133,30 @@ def mouse_event_click_pre(coordinates_ori=[], clicks_ori=[]):
     clicks = clicks_ori.copy()
     click_pre_list = []
     tmp = []
+    time_close = 500
+    time_tmp = coordinates[0]['time']
+    for click in clicks:
+        i = 0
+        for coordinate in coordinates:
+            if(coordinate['time'] <= click['time'] and coordinate['time'] >= click['time'] - time_close):
+                if(coordinate['time'] - time_tmp <= 1000):
+                    click_pre_list.append(coordinate['time'] - time_tmp)
+                    tmp.append(coordinate['time'])
+            if(coordinate['time'] > click['time']):
+                break
+            time_tmp = coordinate['time']
+            i += 1
+        for j in range(i):
+            del coordinates[0]
+    # time_close以内のマウスイベントの数を返す
+    return(len(click_pre_list))
+
+# クリックイベント直後のマウスのスピードを返す関数
+def mouse_speed_click_pre(coordinates_ori=[], clicks_ori=[]):
+    coordinates = coordinates_ori.copy()
+    clicks = clicks_ori.copy()
+    click_pre_list = []
+    tmp = []
     time_close = 100
     time_tmp = coordinates[0]['time']
     for click in clicks:
@@ -148,11 +172,16 @@ def mouse_event_click_pre(coordinates_ori=[], clicks_ori=[]):
             i += 1
         for j in range(i):
             del coordinates[0]
-        # print('click_pre_list : ', len(click_pre_list))
-        # print('coordinates_list : ', len(coordinates))
-        # print('----------------------------------')
     # time_close以内のマウスイベントの数を返す
-    return(len(click_pre_list))
+    # return(len(click_pre_list))
+
+    # time_close以内のマウス速度の平均を返す
+    time_sum = 0
+    j = 0
+    for click_pre in click_pre_list:
+        time_sum += click_pre
+        j += 1
+    return(time_sum / j)
 
 
 data_set = []
