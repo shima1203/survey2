c9c2b02c5baf2ada57e6f3d362493625a33eeb71
shimamamamamamae <shima.k.1203@gmail.com>
2023-01-10 15:38:05 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -84,7 +84,7 @@ def mouse_speed(coordinates=[]):
     i = 0
     time_tmp  = coordinates[0]['time']
     for coordinate in coordinates:
-        if(coordinate['time'] <= 1000):             # １秒以内でイベントが発生している場合、マウスが連続で動いていると考える
+        if(coordinate['time'] - time_tmp <= 1000):             # １秒以内でイベントが発生している場合、マウスが連続で動いていると考える
             sum_mous += coordinate['time'] - time_tmp
             i += 1
         time_tmp  = coordinate['time']
