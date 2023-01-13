4d0af3681e81bd8cafb05a7096ada1ae845a5f1b
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2023-01-14 01:58:19 +0900

    Update

--- function.py
+++ function.py
@@ -39,8 +39,9 @@ def mouse_speed(stop_time , coordinates_ori=[], scroll_ori = []):
         for j in range(i):
             del scroll[0]
         print(rear_scroll['x'])
-        if(coordinate['x'] != rear_scroll['x'] or coordinate['y'] != rear_scroll['y']):
-            if(coordinate_tmp['x'] != coordinate['x'] or coordinate_tmp['y'] != coordinate['y']):
+        
+        if(coordinate_tmp['x'] != coordinate['x'] or coordinate_tmp['y'] != coordinate['y']):
+            if(coordinate['x'] != rear_scroll['x'] or coordinate['y'] != rear_scroll['y']):
                 if(coordinate['time'] - coordinate_tmp["time"] <= stop_time):             # stop_time以内でイベントが発生している場合、マウスが連続で動いていると考える
                     distance += abs(coordinate['x'] - coordinate_tmp['x']) + abs(coordinate['y'] - coordinate_tmp['y'])
                     move_time += coordinate['time'] - coordinate_tmp["time"]
