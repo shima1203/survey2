a3060863d5f496073eafcc238b046bc4e9857efb
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2023-01-14 20:49:10 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -227,7 +227,7 @@ plt.boxplot(la, showmeans=True, boxprops=dict(color='black', linewidth=1),median
 plt.boxplot(lb, showmeans=True, boxprops=dict(color='blue', linewidth=1),medianprops=dict(color='blue', linewidth=1),whiskerprops=dict(color='blue', linewidth=1),capprops=dict(color='blue', linewidth=1),flierprops=dict(markeredgecolor='blue', markeredgewidth=1))
 # df_a.plot.box()
 # df_b.plot.box()
-plt.ylim(0, 4)
+plt.ylim(-0.5, 3.5)
 
 
 
