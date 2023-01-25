fbf50850b659d062aa76acb026f34d1dae60287b
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2023-01-26 01:43:47 +0900

    Update

--- function.py
+++ function.py
@@ -116,7 +116,7 @@ def mouse_speed_click_pre(start_time, finish_time, coordinates_ori=[], clicks_or
     # 最小のマウス速度pick_num個をピックし、その平均を返す
     pick_num = 10
     time_sum = 0
-    click_pre_list_sorted = sorted(click_pre_list, reverse=False)
+    click_pre_list_sorted = sorted(click_pre_list, reverse=True)
     # False:min True:max
     if(len(click_pre_list_sorted) >= pick_num):
         for j in range(pick_num):
