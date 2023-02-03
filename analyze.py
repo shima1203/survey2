e555420f3f0b03b750f097afdbed72133d81674c
shimamamamamamae <107593704+shimamamamamamae@users.noreply.github.com>
2023-02-03 15:38:32 +0900

    Update

--- analyze.py
+++ analyze.py
@@ -301,7 +301,10 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 sns.boxplot(x='variable', y='value', data=df_marged, hue='species', palette='Dark2', ax=ax)
 
-ax.set_xlim([1000,0])
+params = {'legend.fontsize': 25,
+        'legend.handlelength': 3}
+
+plt.rcParams.update(params)
 ax.set_xlabel('time  [ms]')
 ax.set_ylabel('ratio')
 # ax.set_ylim(-0.2, 4.0)
